9d6487eab6b6741fb94ba1551edda5b80e603e33
Chad Oehlschlaeger-Browne <chadnobrowne@gmail.com>
2016-02-09 20:45:38 -0800

    added a useless .reduce call to meet requirement because I can't think of a reason to use it in this site changed .fetchAll to use .map instead of .forEach enclosed script.js in a iife exported the Project constructor with its methods to the window

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,49 +1,47 @@
-$(function() {
-
-  (function projectCreator() {
+(function(module) {
 
     //project Constructor function
-    function Project (project) {
-      this.projectPicture = project.projectPicture;
-      this.projectTitle = project.projectTitle;
-      this.projectDate = project.projectDate;
-      this.projectLink = project.projectLink;
-    }
-    Project.projectList = [];
-    //code to create a method to check localStorage for json object and prepend it to the dom
-    Project.fetchAll = function() {
-      var dataList;
-      if (localStorage.storageProjects) {
+  function Project (project) {
+    this.projectPicture = project.projectPicture;
+    this.projectTitle = project.projectTitle;
+    this.projectDate = project.projectDate;
+    this.projectLink = project.projectLink;
+  }
+  Project.projectList = [];
+  //code to create a method to check localStorage for json object and prepend it to the dom
+  Project.fetchAll = function() {
+    var dataList;
+    if (localStorage.storageProjects) {
+      dataList = JSON.parse(localStorage.storageProjects);
+      Project.projectList = dataList.map(function(project) {
+        $('#projects').prepend(new Project(project).toHtml());
+      });
+      console.log(Project.projectList);
+    } else {
+      $.getJSON('Data/projectData.json', function(data) {
+        localStorage.setItem('storageProjects', JSON.stringify(data));
         dataList = JSON.parse(localStorage.storageProjects);
-        dataList.forEach(function(project) {
-          Project.projectList.push(new Project(project));
+        Project.projectList = dataList.map(function(project) {
+          $('#projects').prepend(new Project(project).toHtml());
         });
-        console.log(Project.projectList);
-        Project.projectList.forEach(function(project) {
-          $('#projects').prepend(project.toHtml());
-        });
-      } else {
-        $.getJSON('Data/projectData.json', function(data) {
-          localStorage.setItem('storageProjects', JSON.stringify(data));
-          dataList = JSON.parse(localStorage.storageProjects);
-          dataList.forEach(function(project) {
-            Project.projectList.push(new Project(project));
-          });
-          Project.projectList.forEach(function(project) {
-            $('#projects').prepend(project.toHtml());
-          });
-        });
-      }
-    };
+      });
+    }
+  };
 
-    //function to create a new project using an existing template and filling in spots with the newly constructed project above
-    Project.prototype.toHtml = function(thisFunction) {
-      var template = Handlebars.compile($('#project-template').text());
-      console.log('yes');
-      return template(this);
-    };
-    Project.fetchAll();
-  })();
+  //function to create a new project using an existing template and filling in spots with the newly constructed project above
+  Project.prototype.toHtml = function(thisFunction) {
+    var template = Handlebars.compile($('#project-template').text());
+    return template(this);
+  };
+  Project.fetchAll();
+
+  //useless code to include reduce because I can't think of any reason why I would use it in this site
+  var arr = ['Hi, ', 'my ', 'name ', 'is ', 'Chad.'];
+  var str = '';
+  str = arr.reduce(function(a, b) {
+    return str + a + b;
+  });
+  console.log(str);
 
   //code for using Nav bar
   $('#nav').on('click', '.nav-item', function(e) {
@@ -54,4 +52,6 @@ $(function() {
   });
   //make home the default page
   $('#nav-list li:first-child').click();
-});
+
+  module.Project = Project;
+})(window);

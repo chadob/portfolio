faafad67d452abc4ddf62ef4e5ec606db877eb7e
chadob <chadnobrowne@gmail.com>
2016-02-18 20:11:34 -0800

    added next

--- Scripts/sectionController.js
+++ Scripts/sectionController.js
@@ -2,7 +2,7 @@
   var sectionController = {};
 
   // Done: Define a function that hides all main section elements, and then reveals just the #about section:
-  sectionController.index = function(ctx) {
+  sectionController.index = function(ctx, next) {
     $('.swapped-body-content').hide();
     var clickedSection = ctx.params.section;
     $('.' + clickedSection).show();
